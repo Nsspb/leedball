ac588c6e57aaacc97199f3e7ea00e6114306473d
Konstantin <whitehorse.spb@gmail.com>
2021-10-25 11:35:09 +0300

    git add

--- obj.js
+++ obj.js
@@ -1,23 +1,23 @@
-const logg = 'console.log'
-const ggg =  [
-    {
-        id: '1',
-        date: '1608083771934',
-        name: 'ВикториЯ РоЖкова',
-        phone: '+7 670 852 27 67',
-        delivery: '397983, Тюменская область\nАстрахань, 23021 Hilbert площадь',
-        orderTotal: '1274999',
-    },
-    {
-        id: '2',
-        date: '1618840173975',
-        name: 'Никита ЖукоВ',
-        phone: '+3 799 238 11 26',
-        delivery: '910371, Алтайский край\nЧелябинск, 640 Белов ул.',
-        orderTotal: '1434999',
-    }]
 
-    console.log(Object.getOwnPropertyNames(ggg));
+// const ggg =  [
+//     {
+//         id: '1',
+//         date: '1608083771934',
+//         name: 'ВикториЯ РоЖкова',
+//         phone: '+7 670 852 27 67',
+//         delivery: '397983, Тюменская область\nАстрахань, 23021 Hilbert площадь',
+//         orderTotal: '1274999',
+//     },
+//     {
+//         id: '2',
+//         date: '1618840173975',
+//         name: 'Никита ЖукоВ',
+//         phone: '+3 799 238 11 26',
+//         delivery: '910371, Алтайский край\nЧелябинск, 640 Белов ул.',
+//         orderTotal: '1434999',
+//     }]
+
+//     console.log(Object.getOwnPropertyNames(ggg));
 
     // const findTodo = function(www, name){
     //     const index = www.find(function(todo, index){
